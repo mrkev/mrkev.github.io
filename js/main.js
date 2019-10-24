619d28ffd7d7e25cbe6303b91cb5442831a5aac9
Kevin Chavez <aykev@fb.com>
2019-10-24 00:19:13 -0700

    add p5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
-var rasterScript = document.createElement("script");
-rasterScript.setAttribute("src", "/js/raster.js");
-rasterScript.setAttribute("type", "text/paperscript");
-rasterScript.setAttribute("canvas", "canvas-1");
-document.body.appendChild(rasterScript);
+function e(type, attrs) {
+  var elem = document.createElement(type);
+  Object.keys(attrs).forEach(function(key) {
+    elem.setAttribute(key, attrs[key]);
+  });
+  return elem;
+}
+
+function script(src, attrs) {
+  return e("script", { ...attrs, src });
+}
+
+//////////////////
+
+function sketch1() {
+  // Append canvas for paper
+  document.getElementById("exp").appendChild(
+    e("canvas", {
+      resize: "true",
+      hidpi: "off",
+      id: "canvas-1"
+    })
+  );
+  // Append paperscript
+  document.body.appendChild(
+    script("/js/raster.js", {
+      type: "text/paperscript",
+      canvas: "canvas-1"
+    })
+  );
+}
+
+function sketch2() {
+  document.body.appendChild(script("/js/p5test.js"));
+}
+
+var sketches = [sketch1, sketch2];
+var rand = Math.floor(Math.random() * sketches.length);
+sketches[rand]();
